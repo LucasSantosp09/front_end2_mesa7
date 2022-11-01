dd6057dc2690dc778496558a0ef1bceefbfcd020
LucasSantosp09 <lucassantos.p09@gmail.com>
2022-11-01 19:59:38 -0300

    Mesa 7 Front-End II

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+    boasvindas()
+}
+function boasvindas(){
+    alert("Página Foi Carregada")
+}
+
+function exibeIdade(){
+    let idade = document.getElementById("idade");
+    alert(idade.value);
+}
+
+function mudaCor(){
+    let titulo = document.getElementById("titulo");
+    titulo.style.color = "red";
+}
+
+
